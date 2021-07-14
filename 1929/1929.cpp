a2e6707e04dd28dffbd72d23a0baa1abff89a4ff
KylusheL <ez2dj1010@naver.com>
2021-07-14 13:57:46 +0900

    2021-07-14 13:57

--- 1929/1929.cpp
+++ 1929/1929.cpp
@@ -3,25 +3,27 @@
 using namespace std;
 
 int main() {
-    int M, N;
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int M, N, i, j;
     cin >> M >> N;
-    int arr[1000001] = {0, };
 
-    arr[1] = 1;
+    bool arr[1000001];
+    for(i = 2; i < 1000001; i++) arr[i] = true;
+    arr[0] = arr[1] = false;
 
-    for(int i = 2; (i * i) <= N; i++) {
-        if(!arr[i]) {
-            for(int j = i * i; j <= N; j += i) {
-                arr[j] = 1;
+    for(i = 2; (i * i) <= N; i++) {
+        if(arr[i]) {
+            for(j = i * i; j <= N; j += i) {
+                arr[j] = false;
             }
         }
     }
 
-    for(int i = M; i <= N; i++) {
-        if(!arr[i])
-            cout << i << endl;
+    for(i = M; i <= N; i++) {
+        if(arr[i])
+            cout << i << '\n';
     }
-
 
     return 0;
 }
---
+++ 4948/4948.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main {
+    bool[123456] = {false, };
+    return 0;
+}
